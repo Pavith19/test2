35c3b66705595d68e6218b849a869f1efff2acaa
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-11 19:15:02 +0530

    Create pro.java

---
+++ pro.java
@@ -0,0 +1,17 @@
+public class FactorialExample {
+
+    // Recursive method to calculate factorial
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        long fact = factorial(number);
+        System.out.println("Factorial of " + number + " = " + fact);
+    }
+}
